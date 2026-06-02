957374477b80f517c91400cbfb39d2b56a770e4c
davidpacecode <davidpace@gmail.com>
2026-06-01 23:42:45 -0500

    have inline editing of titles working. need to style form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-  before_action :set_entry, only: %i[ show edit update destroy ]
+  before_action :set_entry, only: %i[ show edit edit_title update update_title destroy ]
 
   # GET /entries or /entries.json
   def index
@@ -26,6 +26,10 @@ class EntriesController < ApplicationController
   def edit
   end
 
+  # GET /entries/1/edit_title
+  def edit_title
+  end
+
   # POST /entries or /entries.json
   def create
     @entry = Entry.new(entry_params)
@@ -46,6 +50,19 @@ class EntriesController < ApplicationController
     respond_to do |format|
       if @entry.update(entry_params)
         format.html { redirect_to @entry, notice: "Entry was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @entry }
+      else
+        format.html { render :edit, status: :unprocessable_content }
+        format.json { render json: @entry.errors, status: :unprocessable_content }
+      end
+    end
+  end
+
+  # PATCH/PUT /entries/1 or /entries/1.json
+  def update_title
+    respond_to do |format|
+      if @entry.update(entry_params)
+        format.html { redirect_to entries_path, notice: "Entry was successfully updated.", status: :see_other }
         format.json { render :show, status: :ok, location: @entry }
       else
         format.html { render :edit, status: :unprocessable_content }
